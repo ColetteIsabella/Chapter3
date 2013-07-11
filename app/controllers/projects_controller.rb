653b91683fd99f6fe4a726ef6f57090c0908f474
Colette Taylor <coletteisabella@gmail.com>
2013-07-11 14:33:58 -0700

    Added the ability to view a list of all projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,12 @@
 class ProjectsController < ApplicationController
 	def index
-	end
-	
+    @projects = Project.all
+end
 	def new
 		@project = Project.new
 	end
 	def create
-  		@project = Project.new(params[:project]) 
+  		@project = Project.new(params[:project])
   if @project.save
     flash[:notice] = "Project has been created."
     redirect_to @project
