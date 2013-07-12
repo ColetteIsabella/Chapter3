d972c262c3f652c34b4059b3f2aa6b1b023519fe
Colette Taylor <coletteisabella@gmail.com>
2013-07-11 17:27:26 -0700

    Why no worky?
    Just trying to get it right for editing

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,5 +18,17 @@ end
   def show
   	@project = Project.find(params[:id])
   end
+
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been updated."
+      render :action => "edit"
+    end
+  end
 end
 end
