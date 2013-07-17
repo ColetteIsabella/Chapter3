e34a3c9ed72afdf9e552c30bed5a14c79d544b5a
Colette Taylor <coletteisabella@gmail.com>
2013-07-17 15:20:01 -0700

    Associate users to tickets when tickets are created

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,7 +1,6 @@
 Ticketee::Application.configure do
-  config.action_mailer.default_url_options = {
-    :host => "localhost:3000"
-  }
+  config.action_mailer.default_url_options =
+  { :host => "localhost:3000" }
 
   # Settings specified here will take precedence over those in config/application.rb
 
