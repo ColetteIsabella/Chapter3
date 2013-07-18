9fa0437990290992cf16992c67051a46563db597
Colette Taylor <coletteisabella@gmail.com>
2013-07-17 18:07:06 -0700

    Restrict access to project actions to admins only

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,17 +7,17 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
-RSpec.configure do |config|
-  config.before do
-    ActionMailer::Base.deliveries.clear
-  end
-end
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
+  config.before do
+    ActionMailer::Base.deliveries.clear
+  end
+
   config.include EmailSpec::Helpers
+  config.include Devise::TestHelpers, :type => :controller
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
